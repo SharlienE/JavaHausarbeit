6f12ff98a71a3dc6966f50ec8f8d712c4fcea051
Kathi <infosi123aks@gmail.com>
2020-05-10 19:21:11 +0200

    Erstellen der benötigten Methoden für Klasse Ball

--- Ball.java
+++ Ball.java
@@ -34,9 +34,11 @@ import java.awt.geom.*;
        private int durchmesser; // Durchmesser des Balls
        private int xPosition; // xPosition des Balls
        private int yPosition; // yPosition des Balls
-       private final int bodenhoehe; // y-Position des Bodens
+       private final int bodenhoehe; // = 400???; // y-Position des Bodens
        private Canvas leinwand; 
        private int yGeschwindigkeit = 1; // anfängliche Abwärtsgeschwindigkeit
+       private int xGeschwindigkeit = 1; // ???
+       private int wurfrichtung; //???
                 
                 
         /**
@@ -46,7 +48,7 @@ import java.awt.geom.*;
         * 
         */
         public Ball(int xPos, int yPos, int durchmesserball, Color ballfarbe, 
-                    int bodenposition, Canvas leinwandBall)
+                    int bodenposition, Canvas leinwandBall, int r)
          {
            //"zeichengrund" = leinwandBall
                     
@@ -56,14 +58,16 @@ import java.awt.geom.*;
            durchmesser = durchmesserball;
            bodenhoehe = bodenposition;
            leinwand = leinwandBall;
-                    
+           wurfrichtung = r;
+           // Hat Ball auch Attribute Geschwindigkeit / Stoßrichtung??
+           // --> Bezug zu Scannnereingabe
          }
          
-     /**
-     * Zeichne den Ball an der aktuellen Position auf der Leinwand. 
-     *
-     * @param 
-     */
+    /**
+    * Zeichne den Ball an der aktuellen Position auf der Leinwand. 
+    *
+    * 
+    */
     public void zeichneBall()
     {
         leinwand.setForegroundColor(farbe);
@@ -73,7 +77,7 @@ import java.awt.geom.*;
     /**
      * Lösche den Ball an der aktuellen Position auf der Leinwand.
      *
-     * @param  
+     * 
      */
     public void loescheBall()
     {
@@ -83,15 +87,136 @@ import java.awt.geom.*;
     /**
      * Bewege den Ball 
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * 
      */
     public void bewegeBall()
     {
-        //
+        //wenn eingabe="s"
+        
+        
+        // An der aktuellen Position von der Leinwand entfernen.
+        loescheBall();
+            
+        // Neue Position berechnen.
+        yGeschwindigkeit += gravitation;
+        xGeschwindigkeit += gravitation;
+        yPosition += yGeschwindigkeit;
+        xPosition += xGeschwindigkeit;
+
+        // Prüfen, ob der Boden erreicht ist.
+        while(yPosition > (bodenhoehe - durchmesser) && yGeschwindigkeit > 0 && xGeschwindigkeit > 0) {
+            yPosition = (int) (bodenhoehe - durchmesser);
+            yGeschwindigkeit = -yGeschwindigkeit + bremsfaktor; 
+            // An der neuen Position erneut zeichnen.
+            zeichneBall();
+        }
+        if (yPosition == bodenhoehe && xPosition >=60){
+           zeichneBall();
+           // korb = Color rot ???
+        }
+        if (yPosition == bodenhoehe && xPosition < 60) {
+            zeichneBall();
+            // korb = Color grün ???
+        }
+    }
+    
+    /**
+     * Gib die X-Position des Balls.
+     *
+     * @param   xPos
+     * @return  xPos
+     */
+    public int gibXposition (int xPos)
+    {
+        return xPos;
     }
 
+    /**
+     * Gib die Y-Position des Balls.
+     *
+     * @param   yPos
+     * @return  yPos
+     */
+    public int gibYposition (int yPos)
+    {
+        return yPos;
+    }
+
+    /**
+     * Gib die Geschwindigkeit des Balls. 
+     *
+     * @param   yGeschwindigkeit
+     * @param   xGeschwindigkeit
+     * @return    Summe???
+     */
+    public int gibGeschwindigkeit(int yGeschwindigkeit, int xGeschwindigkeit)
+    {
+        
+        // return yGeschwindigkeit, xGeschwindigkeit; ???
+        return yGeschwindigkeit;
+    }
+
+  
+    /**
+     * Gib die Wurfrichtung des Balls. 
+     *
+     * @param     r 
+     * @return    r
+     */
+    public int gibWurfrichtung(int r)
+    {
+        // gestrichelte Linie
+        return r;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void stossrichtungLinks()
+    {
+        // wenn eingabe="l"
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void stossrichtungRechts()
+    {
+        // wenn eingabe="r"
+        
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void erhoeheGeschwindigkeit()
+    {
+        // wenn eingabe="+"
+        
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void reduziereGeschwindigkeit()
+    {
+        // wenn eingabe="-"
+        
+    }
     
     
-         
+
 }       
