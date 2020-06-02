57ac57b5d8d4ab3ff909af353e16782e62265cad
Kathi <infosi123aks@gmail.com>
2020-06-02 09:27:15 +0200

    Klassenkommentierung

--- Spiel.java
+++ Spiel.java
@@ -14,15 +14,16 @@ import java.awt.geom.*;
  */
 public class Spiel
 {
-    // Instanzvariablen 
+    // Deklarieren der Instanzvariablen 
     private Canvas leinwand;
     private Ball ball1; 
     private Wurf wurf1;
-    private Konsole konsole = new Konsole();
-    private String saveEingabe;
+    private Konsole konsole = new Konsole(); //gleichzeitige Objekterzeugung
+    private String saveEingabe; 
 
-    private String defaultprompt = ">";
-    private Scanner scanner = new Scanner(System.in);
+    private String defaultprompt = ">"; // gleichzeitiges Initialisieren
+    // erlaubt dem Spiel, etwas aus System.in zu lesen
+    private Scanner scanner = new Scanner(System.in); //gleichzeitige Objekterzeugung
     
     
     /**
@@ -30,24 +31,39 @@ public class Spiel
      */
     public Spiel()
     {
+        // Leinwand aus Canvas erzeugen (Titel, x, y, Farbe)
         leinwand = new Canvas("Korbwurf",650, 500, Color.white);
+        // sichtbar machen
         leinwand.setVisible(true);
+        // Größe der Leinwand festlegen
         leinwand.setSize(650, 500);
+        // Bodenlinie für Aufprall des Balls definieren
         int boden = 400;
+        // Objekt Ball erzeugen (xPos, yPos, Balldurchmesser, Ballfarbe,
+        // BodenPosition, leinwand)
         ball1 = new Ball (500, 360, 40, Color.yellow, boden, leinwand); 
-        //ball1.zeichneBall(); 
+        // Objekt Wurf erzeugen (xStart, yStart, xEnde, yEnde, v, winkel0,
+        // LinienFarbe, leinwand, ball1)
         wurf1 = new Wurf(520, 380, 400, 250,200, 45, Color.green, leinwand, ball1);
+        // Methode zum Zeichnen des Spielfeldes aufrufen und zeichnen
         zeichneSpielfeld();
+        // Hilfetext in Konsole anzeigen lassen 
         konsole.zeigeHilfetext();
     }
 
     /**
-     * Klassenmethode zum Aufruf des Spiels
+     * Willkommen im Spiel "Korbwurf"
+     * Main-Methode zum Aufruf des Spiels
      * 
-     * @param  String[] Mögliche Parameter (hier keine)
+     * @public - Die Methode kan von außerhalb der Klasse aufgerufen werden
+     * @static - Sie wird aufgerufen, ohne dass vorher ein Objekt der Klasse 
+     *           Spiel erzeugt wurde; keine Instanzmethode
+     * @void   - Kein Rückgabewert
+     * @String[] args - Arrray wird übergeben, welches die Werte der 
+     *                  vom Aufruf entgegengenommenden Kommandozeilen beinhaltet
      */
     public static void main(String[] args){
-        // Instantiierung der Klasse
+        // Implementierung der Programmlogik
         Spiel neuesSpiel = new Spiel();
         neuesSpiel.spielen();
     }
@@ -57,21 +73,31 @@ public class Spiel
      */
     private void zeichneSpielfeld()
     {
+        // Schriftart und -größe festlegen
         leinwand.setFont(new Font("helvetica", Font.BOLD, 14));
+        // Farbe für folgende Anweisungen festlegen
         leinwand.setForegroundColor(Color.BLUE);
+        // Überschrift mit Positionsangaben
         leinwand.drawString("Spiel: Korbwurf", 220, 20);
+        // Rahmenlinien zeichnen
         leinwand.drawLine(0,0,600,0); 
         leinwand.drawLine(600,0,600,400);
         leinwand.drawLine(600,400,0,400);
         leinwand.drawLine(0,400,0,0); 
-
+        
+        // neues Objekt als Rechteck erzeugen (vordefiniert in Canvas)
         Rectangle korb = new Rectangle(0,400,100,20);
+        // mit oben definierter Farbe ausfüllen
         leinwand.fill(korb);
         
+        // Ball am Startpunkt zeichnen
         int boden = 400; 
         ball1.zeichneBallAmAnfang();
-
+        
+        // Wurfrichtung zeichnen
         wurf1.wurfrichtungZeichnen();
+        
+        // Geschwindigkeit anzeigen
         wurf1.vZeichnen();  
     }
     
@@ -83,34 +109,47 @@ public class Spiel
         // In dieser Variable wird der eingelesene String gespeichert, der zurückgegeben wird
         String eingabe;
         
-        // Ausgabe der Eingabeaufforderung
+        // Ausgabe der Eingabeaufforderung, sodass in der Konsole etwas vom 
+        // Benutzer eingegeben werden kann
         System.out.println(prompt);
         
         // einlesen
+        // Scanner gibt die übersprungene Eingabe zurück
         eingabe = scanner.nextLine();
         
-        // Konverieren in kleine Buchstaben und entfernen der führenden und folgenden Leerzeichen
+        // Konverieren in kleine Buchstaben und entfernen der führenden 
+        // und folgenden Leerzeichen
         if (eingabe.length()>0){
             eingabe = eingabe.toLowerCase().trim();
         }
         
+        // Methode beenden und Parameter eingabe zurückgeben
         return eingabe;
     }
     
     /**
      * Durchführung der Kommandos
+     * Wenn ein Kommando vom Benutzer eingegeben wurde, führe die nachfolgenden
+     * Anweisungen aus.
+     * 
+     * @param eingabe - das, was vom Benutzer eingegeben wird
      */
     private void spielen()
     {
+        // lokalen String namens eingabe erzeugen
         String eingabe;
+        // Datentyp boolean namens ende erzeugen und auf false setzen
         boolean ende = false;
         
+       
+        // Kommandos aus Konsole lesen
         eingabe = lesen (">");
         while (!ende){
             switch(eingabe){
                 case("l"):
                 //Wurfrichtung nach links
                     wurf1.wurfrichtungLinks();
+                    // Den Wert der Eingabe im String saveEingabe (zwischen)speichern
                     saveEingabe = eingabe; 
                     break;
                     
@@ -151,17 +190,20 @@ public class Spiel
                     
                 case("q"):
                 //Spielende
-                    ende=true;
+                    ende=true;  // While-Schleife beenden
                     konsole.qEingabe();
-                    System.exit(0);
+                    // Beenden des Programms, indem die virtuelle Maschine beendet wird
+                    // keine weitere Eingabe des Benutzers mehr möglich
+                    // 0 = Status (erfolgreich)
+                    System.exit(0); 
                     break; 
                     
                 default:
-                //Falsches Kommando
+                // Für alle anderen Fälle: falsches Kommando
                 konsole.falscheEingabe();
                     break;
             }
-            eingabe = lesen(">");
+            eingabe = lesen(">"); //Sonst Endlos-Schleife
             if (eingabe.equals("")){
                 // Bei Enter wird das vorherige Kommando ausgeführt
                 eingabe = saveEingabe;
@@ -170,40 +212,49 @@ public class Spiel
     }
     
     /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * Methode, wenn "f" eingegeben wurde
+     * Löschen und erneutes Zeichnen des Spielfeldes.
+     * Der Ball befindet sich wieder am Startpunkt.
+     * Werte aus vorherigen Würfen werden übernommen.
      * 
-     * @param  y	(Beschreibung des Parameters)
-     * @return		(Beschreibung des Rückgabewertes)
      */
     private void neustarten()
     {
-        //Löschen und erneutes Zeichnen des Spielfeldes.
-        //Der Ball ist wieder am Startpunkt.
-        //ball1.loescheBall();
-        //leinwand.wait(20);
+        // Löschen der Leinwand
         leinwand.erase(); 
-        //leinwand.wait(20);
-        //Startwerte aus vorherigem Wurf übernehmen
+        
+        
+        // neuen Ball an Anfangsposition zeichnen
         int boden = 400;
         ball1 = new Ball (500, 360, 40, Color.yellow, boden, leinwand);
+        
+        // Startwerte aus vorherigem Wurf übernehmen
         wurf1 = new Wurf (520, 380, wurf1.getXPunkt(), wurf1.getYPunkt() , 
                wurf1.getGeschwindigkeit(), wurf1.getWinkel(), Color.green, leinwand, ball1);
+        
+        // Spielfeld neu zeichnen
         zeichneSpielfeld();
-        //ball1.zeichneBall();
+        
     }
 
     /**
      * Methode, damit "s" nur einmal ausgeführt werden kann.
      * Danach muss das Spiel neugestartet oder beendet werden.
-     *
-     * @param ERGÄNZEN  
+     * 
+     * @param boden - int Wert der Bodenpositionslinie
+     * 
      */
     private void werfenSpiel()
     {
+        // ersten Wurf ausführen
         wurf1.werfen();
         int boden = 400;
-        String eingabe;
+        
+        // Hilfetext nach der Eingabe von "s" anzeigen
         konsole.zeigeStext();
+        
+        // Eingabe neu lesen und "s" verhindern
+        String eingabe;
         eingabe = lesen (">");
         boolean sEingabe = false; 
         
@@ -229,6 +280,7 @@ public class Spiel
             
                     break;
             }
+            // Damit s nicht erneut bei Enter ausgeführt wird, sonst Endlos-Schleife
             if(!sEingabe)
             {
                 eingabe = lesen(">"); 
